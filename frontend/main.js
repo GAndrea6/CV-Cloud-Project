3850ffc434a6c0c87a81c43418b05378e0c01bd1
GAndrea6 <andrea.garavaglia.98@gmail.com>
2026-09-08 13:12:24 +0200

    Enhance UI and visitor counter styling

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,11 @@ window.addEventListener('DOMContentLoaded', () => {
     getVisitCount();
 });
 
-// Indirizzo dell'API (verrà sostituito dall'URL definitivo dell'Azure Function)
+
 const functionApiUrl = ' https://func-resumegara.azurewebsites.net/api/getresumecounter'; 
 
 function getVisitCount() {
-    // Per ora simuliamo un valore di test finché non creiamo il backend
+    
     fetch('https://func-resumegara.azurewebsites.net/api/GetResumeCounter')
         .then(response => response.json())
         .then(data => {
